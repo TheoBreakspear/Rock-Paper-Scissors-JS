bfbf11b28323eb85eac67d4be7f2e9c63b34629a
TheoBreakspear <theo.breakspear@gmail.com>
2021-07-14 19:57:07 +0100

    tidied white space

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,8 +1,8 @@
 
-//event listners to trigger game play with given move on button click
-document.getElementById('rock').addEventListener("click", function(){playGame('rock')})
-document.getElementById('paper').addEventListener("click", function(){playGame('rock')})
-document.getElementById('scissors').addEventListener("click",function(){playGame('rock')})
+//event listners to trigger game play with given move on button click - this was a cargo cult from stack exchange and should be cleaned up
+document.getElementById('rock').addEventListener("click", function(){playGame('rock')});
+document.getElementById('paper').addEventListener("click", function(){playGame('rock')});
+document.getElementById('scissors').addEventListener("click",function(){playGame('rock')});
 
 //plays game with given move
 function playGame(userChoice) {
@@ -13,25 +13,20 @@ function playGame(userChoice) {
 
 //returns random choice of rock, paper or scissors by computer
 const getComputerChoice = () => {
-   const randomNum = Math.random()
-   const randomInt = Math.floor(3*randomNum)
-   switch (randomInt){
-      case 0:
-         return 'rock';
-         break;
-      case 1:
-         return 'paper';
-         break;
-      case 2:
-         return 'scissors';
-         break;
+   const randomNum = Math.random();
+   const randomInt = Math.floor(3*randomNum);
+   if (randomInt === 0) {
+      return 'rock';
+   } else if (randomInt === 1){
+      return 'paper';
+   } else if (randomInt === 2){
+      return 'scissors';
    }
 }
 
 //gets winner given choice by user and choice by computer
 const getWinner = (userChoice, computerChoice) => {
    switch (userChoice){
-
       case 'rock':
          switch (computerChoice) {
             case 'rock':
@@ -46,33 +41,33 @@ const getWinner = (userChoice, computerChoice) => {
          }
          break;
 
-         case 'paper':
-            switch (computerChoice) {
-               case 'rock':
-                  return 'User';
-                  break;
-               case 'paper':
-                  return 'Tie';
-                  break;
-               case 'scissors':
-                  return 'Computer';
-                  break;
-            }
-            break;
+      case 'paper':
+         switch (computerChoice) {
+            case 'rock':
+               return 'User';
+               break;
+            case 'paper':
+               return 'Tie';
+               break;
+            case 'scissors':
+               return 'Computer';
+               break;
+         }
+         break;
          
-         case 'scissors':
-            switch (computerChoice) {
-               case 'rock':
-                  return 'Computer';
-                  break;
-               case 'paper':
-                  return 'User';
-                  break;
-               case 'scissors':
-                  return 'Tie';
-                  break;
-            }
-            break;   
+      case 'scissors':
+         switch (computerChoice) {
+            case 'rock':
+               return 'Computer';
+               break;
+            case 'paper':
+               return 'User';
+               break;
+             case 'scissors':
+               return 'Tie';
+               break;
+         }
+         break;   
    }
    
 }
@@ -80,10 +75,10 @@ const getWinner = (userChoice, computerChoice) => {
 //prints a message declaring who has won the game
 const declareWinner = (userChoice, computerChoice, outcome) => {
    if (outcome === 'Tie'){
-      console.log(`You chose ${userChoice} and Computer chose ${computerChoice} \n -Game is a tie`);
+      const message = `You chose ${userChoice} and Computer chose ${computerChoice} \n -Game is a tie`;
    } else if (outcome === 'User') {
-      console.log(`You chose ${userChoice} and Computer chose ${computerChoice} \n -User wins!`);
+      const message = `You chose ${userChoice} and Computer chose ${computerChoice} \n -User wins!`;
    } else if (outcome === 'Computer'){
-      console.log(`You chose ${userChoice} and Computer chose ${computerChoice} \n -Computer wins... better luck next time!`);
+      const message = `You chose ${userChoice} and Computer chose ${computerChoice} \n -Computer wins... better luck next time!`;
    }
 }
