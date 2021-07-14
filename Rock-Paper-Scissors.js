4ef5b13d39c9ca9f2c28e612f64f40c411604ada
TheoBreakspear <theo.breakspear@gmail.com>
2021-07-14 14:19:14 +0100

    fixed if in getUserChoice ()

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,6 +1,6 @@
 getUserChoice = userInput => {
     cleanInput = userInput.toLowerCase();
-    if (cleanInput === 'rock' || 'paper' || 'scissors') {
+    if (cleanInput === 'rock' || cleanInput === 'paper' || cleanInput ==='scissors') {
       return cleanInput;
     } else {
       console.log('Invalid input - please chose one of rock, paper, or scissors' )
@@ -8,3 +8,4 @@ getUserChoice = userInput => {
   }
   
   console.log(getUserChoice('scissors'))
+  console.log(getUserChoice('foo')
